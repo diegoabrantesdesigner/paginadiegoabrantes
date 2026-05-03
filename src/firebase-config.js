7e1fcc92a9b7682dbf8f0e77ce6ae259680ef211
Diego Abrantes <contatodiegoabrantes@gmail.com>
2026-05-03 00:53:27 -0300

    fix: build error and clean index.html

---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  projectId: "gen-lang-client-0601795446",
+  appId: "1:667943905520:web:5f4e24b4ac491192313a35",
+  apiKey: "AIzaSyCnw65eFeTguc237AA8_wB_XBZWhond4CQ",
+  authDomain: "gen-lang-client-0601795446.firebaseapp.com",
+  storageBucket: "gen-lang-client-0601795446.firebasestorage.app",
+  messagingSenderId: "667943905520"
+};
+
+export const firestoreDatabaseId = "ai-studio-c9bb9eff-58f2-461d-b179-66a785e18c11";
